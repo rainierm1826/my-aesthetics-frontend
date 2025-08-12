4e2204e9a556d2341b0f0172b7d88a1fa9975868
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2025-08-12 09:16:18 +0800

    fix client dashhboard

--- app/(owner)/owner/dashboard/appointments/page.tsx
+++ app/(owner)/owner/dashboard/appointments/page.tsx
@@ -1,35 +1,10 @@
-"use client"
-
-import {BarChartComponent} from "@/components/BarChartComponent";
-import DashboardCard from "@/components/DashboardCard";
-import DropDownBranch from "@/components/DropDownBranch";
-import LineChartComponent from "@/components/LineChartComponent";
+import AppointmentDashboard from "@/components/AppointmentDashboard";
 import OwnerWrapper from "@/components/OwnerWrapper";
-import PieChartComponent from "@/components/PieChartComponent";
-import ToggleDates from "@/components/ToggleDates";
-
 
 export default function AppointmentsDashboardPage() {
   return (
     <OwnerWrapper title="Appointments Dashboard">
-      <div className="mb-5 flex justify-between items-center">
-        <ToggleDates/>
-        <DropDownBranch/>
-      </div>
-      <div className="flex flex-wrap gap-3 mb-5">
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-        <DashboardCard />
-      </div>
-      <div className="flex flex-col gap-5">
-        <div className="flex flex-wrap gap-3">
-          <PieChartComponent/>
-          <PieChartComponent/>
-        </div>
-        <BarChartComponent/>
-        <LineChartComponent/>
-      </div>
+      <AppointmentDashboard />
     </OwnerWrapper>
   );
 }
---
+++ components/SalesDashboard.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import PieChartComponent from "@/components/PieChartComponent";
+import ToggleDates from "@/components/ToggleDates";
+import {BarChartComponent} from "@/components/BarChartComponent";
+import DashboardCard from "@/components/DashboardCard";
+import DropDownBranch from "@/components/DropDownBranch";
+import LineChartComponent from "@/components/LineChartComponent";
+
+const SalesDashboard = () => {
+  return (
+    <>
+      <div className="mb-5 flex justify-between items-center">
+        <ToggleDates />
+        <DropDownBranch />
+      </div>
+      <div className="flex flex-wrap gap-3 mb-5">
+        <DashboardCard />
+        <DashboardCard />
+        <DashboardCard />
+        <DashboardCard />
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex flex-wrap gap-3">
+          <PieChartComponent />
+          <PieChartComponent />
+        </div>
+        <BarChartComponent />
+        <LineChartComponent />
+      </div>
+    </>
+  );
+};
+
+export default SalesDashboard;
