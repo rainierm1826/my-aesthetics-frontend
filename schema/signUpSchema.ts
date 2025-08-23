7ca3f6d34b4994b3f8603ef9fcdc23b127c72f11
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2025-08-23 18:43:05 +0800

    sign up schema for admin added

--- schema/signUpSchema.ts
+++ schema/signUpSchema.ts
@@ -22,4 +22,38 @@ export const signUpFormSchema = z
     path: ["confirmPassword"],
   });
 
-export type SignInFormValues = z.infer<typeof signUpFormSchema>;
+export type SignUpFormValues = z.infer<typeof signUpFormSchema>;
+
+export const signUpAdminFormSchema = z
+  .object({
+    email: z.email(),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(
+        /[^A-Za-z0-9]/,
+        "Password must contain at least one special character"
+      ),
+    confirmPassword: z
+      .string()
+      .min(8, "Password must be at least 8 characters long"),
+    first_name: z
+      .string()
+      .min(1, "First name is required")
+      .max(100, "First name must be less than 100 characters"),
+    last_name: z
+      .string()
+      .min(1, "Last name is required")
+      .max(100, "First name must be less than 100 characters"),
+    middle_initial: z.string().max(1, "Maximum of one character"),
+    branch_id: z.string().min(1, "Branch is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match.",
+    path: ["confirmPassword"],
+  });
+
+export type SignUpAdminFormValues = z.infer<typeof signUpAdminFormSchema>;
