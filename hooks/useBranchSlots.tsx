504789269c63df7bfe4e11560523621bf80b1d5e
Rainier <marasiganrainier2@gmail.com>
2025-11-28 19:14:52 +0800

    remove use branch slot

--- hooks/useBranchSlots.tsx
+++ hooks/useBranchSlots.tsx
@@ -1,33 +1,33 @@
-"use client";
+// "use client";
 
-import { getBranchAvailableSlots } from "@/api/branch";
-import { AvailableSlotsResponse } from "@/lib/types/aesthetician-types";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+// import { getBranchAvailableSlots } from "@/api/branch";
+// import { AvailableSlotsResponse } from "@/lib/types/aesthetician-types";
+// import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
-export function useBranchSlots({
-  branchId,
-  serviceId,
-  date,
-  token,
-}: {
-  date: string;
-  serviceId: string;
-  branchId: string;
-  token: string;
-}) {
-  return useQuery<AvailableSlotsResponse, Error>({
-    queryKey: ["branch-slots", branchId, serviceId, date],
-    queryFn: () =>
-      getBranchAvailableSlots({
-        branch_id: branchId,
-        service_id: serviceId,
-        date,
-        token,
-      }),
-    enabled: !!branchId && !!serviceId && !!date && !!token,
-    placeholderData: keepPreviousData,
-    refetchOnMount: true,
-    refetchOnWindowFocus: false,
-    staleTime: 30 * 1000, // 30 seconds - shorter stale time for real-time booking
-  });
-}
+// export function useBranchSlots({
+//   branchId,
+//   serviceId,
+//   date,
+//   token,
+// }: {
+//   date: string;
+//   serviceId: string;
+//   branchId: string;
+//   token: string;
+// }) {
+//   return useQuery<AvailableSlotsResponse, Error>({
+//     queryKey: ["branch-slots", branchId, serviceId, date],
+//     queryFn: () =>
+//       getBranchAvailableSlots({
+//         branch_id: branchId,
+//         service_id: serviceId,
+//         date,
+//         token,
+//       }),
+//     enabled: !!branchId && !!serviceId && !!date && !!token,
+//     placeholderData: keepPreviousData,
+//     refetchOnMount: true,
+//     refetchOnWindowFocus: false,
+//     staleTime: 30 * 1000, // 30 seconds - shorter stale time for real-time booking
+//   });
+// }
