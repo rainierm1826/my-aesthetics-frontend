edbfcc613f55f880fc01c02119c06e09bb1b0743
Rainier <marasiganrainier2@gmail.com>
2025-11-28 19:11:22 +0800

    remove useaestheticianslot

--- hooks/useAestheticianSlot.tsx
+++ hooks/useAestheticianSlot.tsx
@@ -1,33 +1,33 @@
-"use client";
+// "use client";
 
-import { getAestheticianSlot } from "@/api/aesthetician";
-import { AvailableSlotsResponse } from "@/lib/types/aesthetician-types";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+// import { getAestheticianSlot } from "@/api/aesthetician";
+// import { AvailableSlotsResponse } from "@/lib/types/aesthetician-types";
+// import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
-export function useAestheticianSlot({
-  aestheticianId,
-  serviceId,
-  date,
-  token,
-}: {
-  date: string;
-  serviceId: string;
-  aestheticianId: string;
-  token: string;
-}) {
-  return useQuery<AvailableSlotsResponse, Error>({
-    queryKey: ["aesthetician-slots", aestheticianId, serviceId, date],
-    queryFn: () =>
-      getAestheticianSlot({
-        aesthetician_id: aestheticianId,
-        service_id: serviceId,
-        date,
-        token,
-      }),
-    enabled: !!aestheticianId && !!serviceId && !!date && !!token,
-    placeholderData: keepPreviousData,
-    refetchOnMount: true,
-    refetchOnWindowFocus: false,
-    staleTime: 30 * 1000, // 30 seconds - shorter stale time for real-time booking
-  });
-}
+// export function useAestheticianSlot({
+//   aestheticianId,
+//   serviceId,
+//   date,
+//   token,
+// }: {
+//   date: string;
+//   serviceId: string;
+//   aestheticianId: string;
+//   token: string;
+// }) {
+//   return useQuery<AvailableSlotsResponse, Error>({
+//     queryKey: ["aesthetician-slots", aestheticianId, serviceId, date],
+//     queryFn: () =>
+//       getAestheticianSlot({
+//         aesthetician_id: aestheticianId,
+//         service_id: serviceId,
+//         date,
+//         token,
+//       }),
+//     enabled: !!aestheticianId && !!serviceId && !!date && !!token,
+//     placeholderData: keepPreviousData,
+//     refetchOnMount: true,
+//     refetchOnWindowFocus: false,
+//     staleTime: 30 * 1000, // 30 seconds - shorter stale time for real-time booking
+//   });
+// }
